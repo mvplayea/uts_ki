b2e03f9ae49d78c44f6e30e040965860ca456c4c
root <root@LAPTOP-QQ6A2F2L.>
2025-05-21 17:12:20 +0700

    first commit

---
+++ src/database/seeders/MedicalRecordMedicineSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MedicalRecordMedicineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
